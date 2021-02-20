06bf6106a606200c30a5b36927604ff4da9d90e8
Akira Shirase <51890190+brokfinger@users.noreply.github.com>
2021-02-20 22:59:41 +0200

    Перенос

--- aggregator/models.py
+++ aggregator/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+# Сейчас этот класс не используется и основное название храниться в
+# классе MangaInfo, но позже будет вынесено отдельно.
+class MangaTitle(models.Model):
+    """
+    Класс для хранения основного и альтернативного названия манги.
+    """
+    pass
+
+
+class MangaInfo(models.Model):
+    """
+    Клвсс хранящий основную информацию об конкретной манге.
+    """
+    title = models.CharField(max_length=255)
+
+
+class MangaLib(models.Model):
+    """
+    Класс для хранения рейтинга, оценки пользователей с сайта
+    https://mangalib.me
+    """
+    pass
+
+
+class MangaRead(models.Model):
+    """
+    Класс для хранения рейтинга, оценки пользователей с сайта
+    https://readmanga.live
+    """
+    pass
